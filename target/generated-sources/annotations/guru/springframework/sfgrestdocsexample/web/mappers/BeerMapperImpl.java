935c27909240beee89137718f567970fc8ed55f9
Gabriel Gitonga <gabriel.gitonga@bskglobaltech.com>
2021-05-28 16:54:03 +0300

    Adding doc with contraints

--- target/generated-sources/annotations/guru/springframework/sfgrestdocsexample/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/guru/springframework/sfgrestdocsexample/web/mappers/BeerMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-05-28T15:46:59+0300",
+    date = "2021-05-28T16:51:27+0300",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 14.0.2 (Oracle Corporation)"
 )
 @Component
